834e451208aa548869ef9182756b5f4e2e2ea09b
Mark J Brewer <Mark.Brewer@bioss.ac.uk>
2014-12-18 18:04:53 +0000

    Create man files for all functions

---
+++ R/plateau-package.R
@@ -0,0 +1,25 @@
+#'  Fit plateau climate envelopes in species distribution models for presence/absence data
+#'
+#'
+#' \tabular{ll}{
+#' Package: \tab plateau\cr
+#' Type: \tab Package\cr
+#' Version: \tab 0.1\cr
+#' Date: \tab 2014-12-18\cr
+#' License: \tab GPL (>= 2)\cr
+#' LazyLoad: \tab yes\cr
+#' }
+#'
+#' \code{\link{plot.envelope}} plots an envelope
+#'
+#' @name plateau-package
+#' @aliases plateau
+#' @docType package
+#' @title  Fit plateau climate envelopes in species distribution models for presence/absence data
+#' @author Mark J Brewer \email{Mark.Brewer@@bioss.ac.uk}
+#' @references
+#' \url{http://www.bioss.ac.uk}
+#' @keywords package
+#' @examples
+#' plot.envelope()
+NULL

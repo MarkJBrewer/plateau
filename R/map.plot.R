d807c9cb5e5ef325e814a8c3e812933790fe183a
Mark J Brewer <Mark.Brewer@bioss.ac.uk>
2015-05-29 17:47:35 +0100

    Allow map.plot to map presence/absences.

--- R/map.plot.R
+++ R/map.plot.R
@@ -1,9 +1,12 @@
 #' Plot maps showing predictions for a given set of climate data.
 #'
 #' \code{map.plot} plots the fitted maps of "climate suitability" for a given
-#' set of climate variable values.
+#' set of climate variable values. Can also be use to plot maps of the presence/absence data.
 #'
-#' @param pars The vector of envelope parameters, length 2p+p+2+p(p-1)/2.
+#' @param inputs If a numeric vector, then it contains the envelope parameters and has length 2p+p+2+p(p-1)/2.
+#' If a logical vector, then it should have length n. The former is used for plotting shaded predictions for
+#' the climate envelope models; the latter is used for making simple presence/absence plots. For presence/absence
+#' plots, \code{x.clim.new} and \code{x.clim} are ignored.
 #' @param x.clim.new A matrix, having the same columns as \code{x.clim}, and
 #' containing the points in climate space at which to evaluate the
 #' envelope and produce predictions in space.
@@ -22,35 +25,49 @@
 #' @return Map of predictions of "climate suitability".
 #' @seealso \code{link{envelope.plot}}
 #' @export
-map.plot <- function(pars,x.clim.new,x.clim,coordinates,species.name="",scenario.name="",
+map.plot <- function(inputs,x.clim.new,x.clim,coordinates,species.name="",scenario.name="",
     save.PDF=FALSE,file.name){
+    plot.type <- "Prediction"
+    if(is.logical(inputs)){
+        plot.type <- "Presences"
+        scenario.name <- "PA"
+    }
     if(save.PDF & missing(file.name)){
         file.name <- paste("Map",species.name,scenario.name,".pdf",sep="")
     }
-    if(missing(x.clim)){
-        x.clim <- x.clim.new
+    if(plot.type=="Prediction"){
+        if(missing(x.clim)){
+            x.clim <- x.clim.new
+        }
+        n.x.clim <- ncol(x.clim.new)
+        # Now standardise the climate variables by mapping onto [0,1]
+        x.clim.std <- x.clim.new
+        x.clim.min <- apply(x.clim,2,min)
+        x.clim.max <- apply(x.clim,2,max)
+        for(i in 1:n.x.clim){
+            x.clim.std[,i] <- (x.clim.new[,i]-x.clim.min[i])/(x.clim.max[i]-x.clim.min[i])
+        }
+        x.envelope <- env.fn(inputs,x.clim.std)$x.envelope
+        plot.p <- 1/(1+exp(x.envelope))
+        plot.p <- round(plot.p*1000)
+        palette(grey(seq(0,1,len=1001)))
+    }else{
+        plot.p <- 15*inputs+1
     }
-    n.x.clim <- ncol(x.clim.new)
-    # Now standardise the climate variables by mapping onto [0,1]
-    x.clim.std <- x.clim.new
-    x.clim.min <- apply(x.clim,2,min)
-    x.clim.max <- apply(x.clim,2,max)
-    for(i in 1:n.x.clim){
-        x.clim.std[,i] <- (x.clim.new[,i]-x.clim.min[i])/(x.clim.max[i]-x.clim.min[i])
-    }
-    x.envelope <- env.fn(pars,x.clim.std)$x.envelope
-    plot.p <- 1/(1+exp(x.envelope))
     if(save.PDF){pdf(file.name)}
     op <- par()
-    plot.p <- round(plot.p*1000)
-    palette(grey(seq(0,1,len=1001)))
     par(mar=c(1,1,1,1)+0.1)
     plot(mapproject(x=coordinates$long,y=coordinates$lat,proj='azequalarea'),
         pch=16,xaxt="n",yaxt="n",col=plot.p,
         xlab="",ylab="")
-    text(-0.226,-0.23,paste(scenario.name,", ",species.name,sep=""),pos=4,cex=2)
-    legend(-0.21,-0.25,legend=c("Probability 0.1","Probability 0.5","Probability 0.9"),pch=c(16,16,16),
-        col=c(900,500,100))
+    if(plot.type=="Prediction"){
+        text(-0.226,-0.23,paste(scenario.name,", ",species.name,sep=""),pos=4,cex=2)
+        legend(-0.21,-0.25,legend=c("Probability 0.1","Probability 0.5","Probability 0.9"),pch=c(16,16,16),
+            col=c(900,500,100))
+    }else{
+        text(-0.226,-0.23,species.name,pos=4,cex=2)
+        legend(-0.2,-0.25,legend=c("Recorded Presence","Recorded Absence"),pch=c(16,1))
+    }
     par(op)
     palette("default")
     if(save.PDF){dev.off()}
